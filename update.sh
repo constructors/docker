f654cfc8374938fbd768281c60dc6300d2473b20
Joris van Rooij <jorrizza@jrrzz.net>
2015-05-22 14:44:04 +0200

    Use the common app container.

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # Updates all container types and publishes the updates
 
 # Container types, dependency order matters
-TYPES="common ruby python-legacy python java"
+TYPES="common common-app ruby python-legacy python java"
 
 for TYPE in $TYPES; do
     make -C "$TYPE" build push clean
